0acfa5ca3607d40fcdbda60e641970fc8f024cd0
felixdomingos1 <felixsdomingos93@gmail.com>
2023-10-21 23:34:33 +0100

    updates

--- routes/register.js
+++ routes/register.js
@@ -2,5 +2,5 @@ import { Router } from 'express';
 const router = Router();
 import { register } from '../controllers/UserController.js';
 
-router.post('/register',register);
+router.post('/register', register);
 export default router;    
