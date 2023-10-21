1cc3c155ecd4448a4f5cb81ebc14b5f64f85fb4f
felixdomingos1 <felixsdomingos93@gmail.com>
2023-10-21 23:40:36 +0100

    updates

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { connect } from 'mongoose';
 import express, { json } from 'express';
 import { json as _json } from 'body-parser';
 import cors from 'cors';
@@ -7,17 +6,22 @@ require('dotenv').config();
 import registerRoute from './routes/register';
 const app = express();
 
-const uri = process.env.DB_URI
-
-connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Database Connected'))
-    .catch(err => console.error('Something went wrong', err));
 
 app.use(_json());
 app.use(cors());
 app.use(json())
 app.use('/api',[registerRoute]);
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server Started on Port: ${port}`)
-});
+
+const connection = async () =>{
+    const port = process.env.PORT || 5000;
+    try {
+        await mongoose.connect(process.env.DB_URI)
+    } catch (err) {
+        console.log(err);
+        
+    }
+    app.listen(port, () => {
+        console.log(`Server Started on Port: ${port}`)
+    });
+  }
+  connection()
